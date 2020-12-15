e620d1e2d7ebbcfd4be48c570d0918294e29c97a
Máté Szendrei <mate.szendrei@stud.uni-corvinus.hu>
2020-12-15 18:10:38 +0100

    excel export

--- IRF_PROJECT/IRF_PROJECT/Form1.cs
+++ IRF_PROJECT/IRF_PROJECT/Form1.cs
@@ -105,12 +105,91 @@ namespace IRF_PROJECT
 
 
             xlSheet = xlWB.ActiveSheet;
-            //CreateTable();
+            CreateTable();
 
             xlApp.Visible = true;
             xlApp.UserControl = true;
         }
+        private void CreateTable()
+        {
+            //fejléc
+            string[] headers = new string[] {
+             "Termék Neve",
+             "Márka",
+             "Ajánlott Nem",
+             "Színe",
+             "Másodlagoskód",
+             "Link"
+            };
 
-        
+            for (int i = 0; i < headers.Length; i++)
+            {
+
+                xlSheet.Cells[1, i + 1] = headers[i];
+
+            }
+            //object[,] values = new object[p.Count, headers.Length];
+            //int szamlalo = 0;
+            //foreach (Penztarca elem in p)
+            //{
+            //    values[szamlalo, 0] = elem.Név;
+            //    values[szamlalo, 1] = elem.Márka;
+            //    values[szamlalo, 2] = elem.Nem;
+            //    values[szamlalo, 3] = elem.Szín;
+            //    values[szamlalo, 4] = elem.Másodlagoskod;
+            //    values[szamlalo,6]=
+            //    szamlalo++;
+
+
+            //}
+
+            //for (int i = 0; i < szamlalo; i++)
+            //{
+
+            //    xlSheet.Cells[2 + i, 1] = values[i, 0];
+            //    xlSheet.Cells[2 + i, 2] = values[i, 1];
+            //    xlSheet.Cells[2 + i, 3] = values[i, 2];
+            //    xlSheet.Cells[2 + i, 4] = values[i, 3];
+            //    xlSheet.Cells[2 + i, 5] = values[i, 4];
+            //    xlSheet.Cells[2 + i, 6] = "valami link" + values[i, 4].ToString();
+
+            //}
+
+            int szamlalo = 0;
+
+            foreach (Penztarca elem in p)
+            {
+                xlSheet.Cells[2 + szamlalo, 1] = elem.Név;
+                xlSheet.Cells[2 + szamlalo, 2] = elem.Márka;
+                xlSheet.Cells[2 + szamlalo, 3] = elem.Nem;
+                xlSheet.Cells[2 + szamlalo, 4] = elem.Szín;
+                xlSheet.Cells[2 + szamlalo, 5] = elem.Másodlagoskod;
+                xlSheet.Cells[2 + szamlalo, 6] = "valami link" + elem.Másodlagoskod;
+
+
+                szamlalo++;
+            }
+
+
+
+
+           
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+            return ExcelCoordinate;
+        }
+
     }
 }
